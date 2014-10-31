3e45fb2e361c8cc20942a74a8f4ec5cd2d1067b9
Andreas Rolén <andreasrolen93@gmail.com>
2014-10-31 15:58:20 +0100

    Large commit with random stuff

--- livescore/livescore.js
+++ livescore/livescore.js
@@ -2,18 +2,48 @@ Teams = new Mongo.Collection("teams");
 Games = new Mongo.Collection("games");
 
 if (Meteor.isClient) {
-  Template.body.helpers({
-    teams: function () {
-      return Teams.find();
-    }
-  });
+	Template.newgame.helpers({
+		teams: function () {
+			return Teams.find();
+		}
+	});
 
-  Template.team.events({
-  	"click .point": function () {
-  		// add one point to the team
-  		Teams.update(this._id, {$inc: {liveScore: 1} });
-  	}
-  });
+	Template.newgame.events({
+		'submit form': function() {
+			var teamOne = event.target.teamOne.value;
+			var teamTwo = event.target.teamTwo.value;
+			var gameType = event.target.gameType.value;
+			Games.insert({
+				createdAt: new Date(),
+				teamOne: teamOne,
+				teamTwo: teamTwo,
+				gameType: gameType
+			});
+		}
+	});
+
+	Template.teamlist.helpers({
+		teams: function () {
+			return Teams.find();
+		}
+	});
+
+	Template.gamelist.helpers({
+		games: function() {
+			return Games.find();
+		}
+	});
+
+	Template.gamelist.events({
+		'click input.delete': function () {
+			Games.remove(this._id);
+		},
+		
+		'click input.start': function() {
+			Router.go("/live/" + this._id);
+		}
+
+	});
 
 }
 
--- livescore/routing.js
+++ livescore/routing.js
@@ -1,9 +1,31 @@
-Router.route('/', function() {
+Router.route ('/live', function() {
+	this.render('live');
+});
 
+Router.route('/live/:_id', function() {
+	var game = Games.findOne({_id: this.params._id});
+	this.render('liveGame', {data: game});
+});
+
+Router.route('/teamlist', function() {
+	this.render('teamlist');
+});
+
+Router.route('/game', function() {
+	this.render('gamelist');
+});
+
+Router.route('/game/new', function() {
+	this.render('newgame');
+});
+
+Router.route('/game/create', {where: 'server'}).post(function() {
+
+	this.render('gamelist');
 });
 
 Router.route('/team/:name/inc', {where: 'server'})
-  .post(function () {
+  .put(function () {
   	this.response.end(this.params.name + ' got 1 point\n');
   	Teams.update({name: this.params.name}, {$inc: {liveScore: 1} });
   });
