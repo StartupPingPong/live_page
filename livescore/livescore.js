44c58f30eb94eac4c6d7368687c45e6157820062
Andreas Rolén <andreasrolen93@gmail.com>
2014-10-31 11:46:51 +0100

    init the database and simple app

--- livescore/livescore.js
+++ livescore/livescore.js
@@ -1,23 +1,11 @@
+Teams = new Mongo.Collection("teams");
+Games = new Mongo.Collection("games");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.body.helpers({
+    teams: function () {
+      return Teams.find();
     }
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
