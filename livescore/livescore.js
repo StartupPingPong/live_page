8db8ca9545759c5df91bb923ec3d775e30c94756
Andreas Rolén <andreasrolen93@gmail.com>
2014-11-07 16:47:58 +0100

    Loading in dummygames in brackets if not full

--- livescore/livescore.js
+++ livescore/livescore.js
@@ -61,15 +61,15 @@ if (Meteor.isClient) {
 			var status = event.target.status.value;
 			Games.update({_id: gameID}, {$set:
 				{teamOne: teamOne.name,
-				teamOneID: teamOne._id,
-				teamOneScore: teamOneScore,
-				teamTwo: teamTwo.name,
-				teamTwoID: teamTwo._id,
-				teamTwoScore: teamTwoScore,
-				gameType: gameType,
-				eventLocation: eventLocation,
-				status: status}
-			});
+					teamOneID: teamOne._id,
+					teamOneScore: teamOneScore,
+					teamTwo: teamTwo.name,
+					teamTwoID: teamTwo._id,
+					teamTwoScore: teamTwoScore,
+					gameType: gameType,
+					eventLocation: eventLocation,
+					status: status}
+				});
 			Router.go('/admin/game');
 		}
 	});
@@ -131,104 +131,136 @@ if (Meteor.isClient) {
 			return Games.find();
 		},
 		groupStageGames: function(eventLocation) {
-			return Games.find({gameType: "Groupstage", eventLocation: eventLocation})
+			var result = Games.find({gameType: "Groupstage", eventLocation: eventLocation})
+			return result;
 		},
-		quarterFinalGames: function(eventLocation) {
-			return Games.find({gameType: "Quarterfinal", eventLocation: eventLocation})
+
+		findGames: function(gameType, eventLocation) {
+			var result = Games.find({gameType: gameType+"final", eventLocation: eventLocation}).fetch();
+			var dummyCount = result.length + 1;
+			var maxCount = 0;
+			switch (gameType) {
+				case "Quarter":
+					maxCount = 5;
+					break;
+				case "Semi":
+					maxCount = 3;
+					break;
+				case "Grand":
+					maxCount = 2;
+					break;
+			}
+			if(result.length < maxCount) {
+				while(dummyCount < maxCount) {
+					var dummy = {
+						_id: gameType+"final"+dummyCount,
+						active: false,
+						createdAt: new Date(),
+						eventLocation: eventLocation,
+						status: "pre",
+						teamOne: gameType+" Finalist #" + ((dummyCount*2)-1),
+						teamOneID: "team"+((dummyCount*2)-1),
+						teamOneScore: 0,
+						teamTwo: gameType+" Finalist #" + (dummyCount*2),
+						teamTwoID: "team"+dummyCount*2,
+						teamTwoScore: 0
+					}
+					result.push(dummy);
+					dummyCount++;
+				}
+			} else if(result.length > (maxCount-1)){
+				return result.slice(0,(maxCount-1));
+			};
+			return result;
 		},
-		semiFinalGames: function(eventLocation) {
-			return Games.find({gameType: "Semifinal", eventLocation: eventLocation})
-		},
-		finalGames: function(eventLocation) {
-			return Games.find({gameType: "Grandfinal", eventLocation: eventLocation})
-		},
+
 		currentLocation: function() {
 			return Session.get('eventLocation');
 		}
 	});
 
-	Template.gamelist.events({
-		'click a': function(event, template) {
-			var eventLocation = $(event.target).data('name');
-			Session.set('eventLocation', eventLocation);
-		},
+Template.gamelist.events({
+	'click a': function(event, template) {
+		var eventLocation = $(event.target).data('name');
+		Session.set('eventLocation', eventLocation);
+	},
 
-		'mouseover .team-item': function(event, template) {
-			var teamID = $(event.target).data('id');
-			$('.' + teamID).addClass("mouse-on");
-		},
+	'mouseover .team-item': function(event, template) {
+		var teamID = $(event.target).data('id');
+		$('.' + teamID).addClass("mouse-on");
+	},
 
-		'mouseleave .team-item': function(event, template) {
-			var teamID = $(event.target).data('id');
-			$('.' + teamID).removeClass("mouse-on");
-		}
-	});
+	'mouseleave .team-item': function(event, template) {
+		var teamID = $(event.target).data('id');
+		$('.' + teamID).removeClass("mouse-on");
+	}
+});
 
-	Template.live.helpers({
-		livegame: function() {
-			return Games.findOne({active: true});
-		}
-	});
+Template.live.helpers({
+	livegame: function() {
+		return Games.findOne({active: true});
+	}
+});
 
-	Template.adminLive.helpers({
-		livegame: function() {
-			return Games.findOne({active: true});
-		}
-	});
+Template.adminLive.helpers({
+	livegame: function() {
+		return Games.findOne({active: true});
+	}
+});
 
-	Template.adminLive.events({
-		'click input.scoreOne': function(event, template) {
-			var gameID = $('#liveGameID').text();
-			Games.update({_id: gameID}, {$inc: {teamOneScore: 1}});
-		},
+Template.adminLive.events({
+	'click input.scoreOne': function(event, template) {
+		var gameID = $('#liveGameID').text();
+		Games.update({_id: gameID}, {$inc: {teamOneScore: 1}});
+	},
 
-		'click input.scoreTwo': function(event, template) {
-			var gameID = $('#liveGameID').text();
-			Games.update({_id: gameID}, {$inc: {teamTwoScore: 1}});
-		}
-	});
+	'click input.scoreTwo': function(event, template) {
+		var gameID = $('#liveGameID').text();
+		Games.update({_id: gameID}, {$inc: {teamTwoScore: 1}});
+	}
+});
 
-	Template.totalScore.helpers({
-		teams: function() {
-			return Teams.find({}, {sort: {totalScore: -1}});
-		}
-	});
+Template.totalScore.helpers({
+	teams: function() {
+		return Teams.find({}, {sort: {totalScore: -1}});
+	}
+});
 
-	Template.adminTotalScore.helpers({
-		teams: function() {
-			return Teams.find({}, {sort: {totalScore: -1}});
-		}
-	});
+Template.adminTotalScore.helpers({
+	teams: function() {
+		return Teams.find({}, {sort: {totalScore: -1}});
+	}
+});
 
-	Template.adminTotalScore.events({
-		'click a': function(event, template) {
-			var teamID = $(event.target).data('id');
-			var newScore = $(event.target.parentNode.previousElementSibling.lastElementChild).val();
-			Teams.update({_id: teamID}, {$set: {totalScore: newScore}});
-			$(event.target.parentNode.previousElementSibling.lastElementChild).val('');
-		}
-	});
+Template.adminTotalScore.events({
+	'click a': function(event, template) {
+		var teamID = $(event.target).data('id');
+		var newScore = $(event.target.parentNode.previousElementSibling.lastElementChild).val();
+		Teams.update({_id: teamID}, {$set: {totalScore: newScore}});
+		$(event.target.parentNode.previousElementSibling.lastElementChild).val('');
+	}
+});
 
-	Template.createTeam.events({
-		'submit form': function(event, template) {
-			event.preventDefault();
-			var teamName = event.target.teamName.value;
-			var teamScore = event.target.teamScore.value;
-			Teams.insert({
-				createdAt: new Date(),
-				name: teamName,
-				totalScore: teamScore,
-				owner: Meteor.userId(),
-				username: Meteor.user().username
-			});
-			Router.go('/admin/total');
-		}
-	});
+Template.createTeam.events({
+	'submit form': function(event, template) {
+		event.preventDefault();
+		var teamName = event.target.teamName.value;
+		var teamScore = event.target.teamScore.value;
+		Teams.insert({
+			createdAt: new Date(),
+			name: teamName,
+			totalScore: teamScore,
+			owner: Meteor.userId(),
+			username: Meteor.user().username
+		});
+		Router.go('/admin/total');
+	}
+});
 
-	Accounts.ui.config({
-		passwordSignupFields: "USERNAME_ONLY"
-	});
-	
+Accounts.ui.config({
+	passwordSignupFields: "USERNAME_ONLY"
+});
+
 }
 
 
